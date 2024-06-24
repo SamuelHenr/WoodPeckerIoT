428743e732711cacc5bf9c17abb0dd10fe6feaaf
Samuel Henrique Miranda Alves <40040561+SamuelHenr@users.noreply.github.com>
2024-06-24 17:51:42 -0300

    Update main.py

--- client/main.py
+++ client/main.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-order = Order()
-
 @app.route('/hello/')
 def hello(name=None):
     return render_template('hello.html', path=url_for('static', filename='sendRequest.js'))
