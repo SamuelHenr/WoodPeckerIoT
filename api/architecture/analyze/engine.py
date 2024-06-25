81f9c75b49057c07f97446022b49cb85959a0bf4
Samuel Henrique Miranda Alves <40040561+SamuelHenr@users.noreply.github.com>
2024-06-25 09:16:06 -0300

    Update engine.py

--- api/architecture/analyze/engine.py
+++ api/architecture/analyze/engine.py
@@ -3,12 +3,16 @@ from architecture.analyze.analyze_interface import *
 class Engine(Analyze):
 
     nodes = {}
-
+    
+    header = ['Packet ID', 'TIME', 'Size', 'eth.src', 'eth.dst', 'IP.src', 'IP.dst', 'IP.proto', 'port.src', 'port.dst']
+    IP_DEVICE = 5
+    TIMESTAMP = 1
+    
     def create_update_node(self, origin: str, last_modified: int):
         self.nodes[origin] = last_modified
 
     def receive_data(self, data: [str]):
         for node in data:
-            self.create_update_node(node[5], node[1])
+            self.create_update_node(node[self.IP_DEVICE], node[self.TIMESTAMP])
 
         return self.nodes
